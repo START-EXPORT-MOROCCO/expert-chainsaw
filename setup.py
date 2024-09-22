fc968c2bb62c614d1fe17156203b5c912078130f
aka <157433003+aka2024@users.noreply.github.com>
2024-09-22 20:41:50 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my_package",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="BARKI MUSTAPHA @aka2024",
+    author_email="startexpor@gmail.com",
+    description="A python app",
+    url="https://github.com/aka2024/my-python-package",
+)
